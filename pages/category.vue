019582ea0afb7a9f44bb28aada39e90ebca245d9
chhai-cryto <chhai.dev@gmial.com>
2026-02-17 16:26:22 +0700

    feat: add category page 10%

---
+++ pages/category.vue
@@ -0,0 +1,10 @@
+<template>
+    <div data-theme="light" class="min-h-screen bg-slate-50">
+    <section class="section container">
+        <h1 class=" bg-[#FFFFFF] text-3xl font-bold">
+           Top Job Categories to Work in Cambodia
+        </h1>
+    </section>
+  </div>
+</template>
+ 
