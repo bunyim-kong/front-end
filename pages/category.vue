92c74cae3e6304603104a37aac67cb39b9a8470c
chhai-cryto <chhai.dev@gmial.com>
2026-02-18 15:37:16 +0700

    feat: category page 30%

--- pages/category.vue
+++ pages/category.vue
@@ -4,6 +4,184 @@
         <h1 class=" bg-[#FFFFFF] text-3xl font-bold">
            Top Job Categories to Work in Cambodia
         </h1>
+      
+        
+
+
+    <div class="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6 mt-10">
+
+    <!-- CARD -->
+    <a href="sales.html" 
+       class="relative bg-white border border-gray-200 rounded-lg p-6 hover:shadow-md transition">
+
+      <!-- Number -->
+      <div class="absolute top-4 right-4 bg-purple-100 text-purple-600 text-sm px-3 py-1 rounded">
+        583
+      </div>
+
+      <!-- Icon -->
+      <div class="text-purple-600 text-5xl mb-6">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-10">
+            <path fill-rule="evenodd" d="M7.5 5.25a3 3 0 0 1 3-3h3a3 3 0 0 1 3 3v.205c.933.085 1.857.197 2.774.334 1.454.218 2.476 1.483 2.476 2.917v3.033c0 1.211-.734 2.352-1.936 2.752A24.726 24.726 0 0 1 12 15.75c-2.73 0-5.357-.442-7.814-1.259-1.202-.4-1.936-1.541-1.936-2.752V8.706c0-1.434 1.022-2.7 2.476-2.917A48.814 48.814 0 0 1 7.5 5.455V5.25Zm7.5 0v.09a49.488 49.488 0 0 0-6 0v-.09a1.5 1.5 0 0 1 1.5-1.5h3a1.5 1.5 0 0 1 1.5 1.5Zm-3 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clip-rule="evenodd" />
+            <path d="M3 18.4v-2.796a4.3 4.3 0 0 0 .713.31A26.226 26.226 0 0 0 12 17.25c2.892 0 5.68-.468 8.287-1.335.252-.084.49-.189.713-.311V18.4c0 1.452-1.047 2.728-2.523 2.923-2.12.282-4.282.427-6.477.427a49.19 49.19 0 0 1-6.477-.427C4.047 21.128 3 19.852 3 18.4Z" />
+        </svg>
+
+      </div>
+
+      <!-- Title -->
+      <h3 class="font-semibold text-gray-800">
+        Sales, Business Development
+      </h3>
+    </a>
+
+
+    <a href="finance.html" 
+       class="relative bg-white border border-gray-200 rounded-lg p-6 hover:shadow-md transition">
+
+      <div class="absolute top-4 right-4 bg-purple-100 text-purple-600 text-sm px-3 py-1 rounded">
+        234
+      </div>
+
+      <div class="text-purple-600 text-5xl mb-6">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-10">
+        <path fill-rule="evenodd" d="M6.32 1.827a49.255 49.255 0 0 1 11.36 0c1.497.174 2.57 1.46 2.57 2.93V19.5a3 3 0 0 1-3 3H6.75a3 3 0 0 1-3-3V4.757c0-1.47 1.073-2.756 2.57-2.93ZM7.5 11.25a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H8.25a.75.75 0 0 1-.75-.75v-.008Zm.75 1.5a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H8.25Zm-.75 3a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H8.25a.75.75 0 0 1-.75-.75v-.008Zm.75 1.5a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75V18a.75.75 0 0 0-.75-.75H8.25Zm1.748-6a.75.75 0 0 1 .75-.75h.007a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75h-.007a.75.75 0 0 1-.75-.75v-.008Zm.75 1.5a.75.75 0 0 0-.75.75v.008c0 .414.335.75.75.75h.007a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75h-.007Zm-.75 3a.75.75 0 0 1 .75-.75h.007a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75h-.007a.75.75 0 0 1-.75-.75v-.008Zm.75 1.5a.75.75 0 0 0-.75.75v.008c0 .414.335.75.75.75h.007a.75.75 0 0 0 .75-.75V18a.75.75 0 0 0-.75-.75h-.007Zm1.754-6a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75h-.008a.75.75 0 0 1-.75-.75v-.008Zm.75 1.5a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75h-.008Zm-.75 3a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75h-.008a.75.75 0 0 1-.75-.75v-.008Zm.75 1.5a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75V18a.75.75 0 0 0-.75-.75h-.008Zm1.748-6a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75h-.008a.75.75 0 0 1-.75-.75v-.008Zm.75 1.5a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75h-.008Zm-8.25-6A.75.75 0 0 1 8.25 6h7.5a.75.75 0 0 1 .75.75v.75a.75.75 0 0 1-.75.75h-7.5a.75.75 0 0 1-.75-.75v-.75Zm9 9a.75.75 0 0 0-1.5 0V18a.75.75 0 0 0 1.5 0v-2.25Z" clip-rule="evenodd" />
+        </svg>
+
+      </div>
+
+      <h3 class="font-semibold text-gray-800">
+        Finance, Accounting, Audit
+      </h3>
+    </a>
+
+
+    <a href="it.html" 
+       class="relative bg-white border border-gray-200 rounded-lg p-6 hover:shadow-md transition">
+
+      <div class="absolute top-4 right-4 bg-purple-100 text-purple-600 text-sm px-3 py-1 rounded">
+        227
+      </div>
+
+      <div class="text-purple-600 text-5xl mb-6">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-10">
+        <path fill-rule="evenodd" d="M2.25 6a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V6Zm3.97.97a.75.75 0 0 1 1.06 0l2.25 2.25a.75.75 0 0 1 0 1.06l-2.25 2.25a.75.75 0 0 1-1.06-1.06l1.72-1.72-1.72-1.72a.75.75 0 0 1 0-1.06Zm4.28 4.28a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z" clip-rule="evenodd" />
+        </svg>
+
+      </div>
+
+      <h3 class="font-semibold text-gray-800">
+        IT Hardware, Software
+      </h3>
+    </a>
+
+
+    <a href="customer.html" 
+       class="relative bg-white border border-gray-200 rounded-lg p-6 hover:shadow-md transition">
+
+      <div class="absolute top-4 right-4 bg-purple-100 text-purple-600 text-sm px-3 py-1 rounded">
+        217
+      </div>
+
+      <div class="text-purple-600 text-5xl mb-6">🎧</div>
+
+      <h3 class="font-semibold text-gray-800">
+        Customer Service, Support
+      </h3>
+    </a>
+
+
+    <a href="marketing.html" 
+       class="relative bg-white border border-gray-200 rounded-lg p-6 hover:shadow-md transition">
+
+      <div class="absolute top-4 right-4 bg-purple-100 text-purple-600 text-sm px-3 py-1 rounded">
+        168
+      </div>
+
+      <div class="text-purple-600 text-5xl mb-6">▶️</div>
+
+      <h3 class="font-semibold text-gray-800">
+        Marketing, Media, Creative
+      </h3>
+    </a>
+
+
+    <a href="logistics.html" 
+       class="relative bg-white border border-gray-200 rounded-lg p-6 hover:shadow-md transition">
+
+      <div class="absolute top-4 right-4 bg-purple-100 text-purple-600 text-sm px-3 py-1 rounded">
+        70
+      </div>
+
+      <div class="text-purple-600 text-5xl mb-6">🚚</div>
+
+      <h3 class="font-semibold text-gray-800">
+        Logistics, Warehousing, Port
+      </h3>
+    </a>
+
+
+    <a href="hr.html" 
+       class="relative bg-white border border-gray-200 rounded-lg p-6 hover:shadow-md transition">
+
+      <div class="absolute top-4 right-4 bg-purple-100 text-purple-600 text-sm px-3 py-1 rounded">
+        69
+      </div>
+
+      <div class="text-purple-600 text-5xl mb-6">👥</div>
+
+      <h3 class="font-semibold text-gray-800">
+        HR, Training and Recruitment
+      </h3>
+    </a>
+
+
+    <a href="engineering.html" 
+       class="relative bg-white border border-gray-200 rounded-lg p-6 hover:shadow-md transition">
+
+      <div class="absolute top-4 right-4 bg-purple-100 text-purple-600 text-sm px-3 py-1 rounded">
+        63
+      </div>
+
+      <div class="text-purple-600 text-5xl mb-6">👷</div>
+
+      <h3 class="font-semibold text-gray-800">
+        Engineering, Technical, HSE
+      </h3>
+    </a>
+
+
+    <a href="driver.html" 
+       class="relative bg-white border border-gray-200 rounded-lg p-6 hover:shadow-md transition">
+
+      <div class="absolute top-4 right-4 bg-purple-100 text-purple-600 text-sm px-3 py-1 rounded">
+        50
+      </div>
+
+      <div class="text-purple-600 text-5xl mb-6">🚗</div>
+
+      <h3 class="font-semibold text-gray-800">
+        Driver, Delivery, Motorbike
+      </h3>
+    </a>
+
+
+    <a href="admin.html" 
+       class="relative bg-white border border-gray-200 rounded-lg p-6 hover:shadow-md transition">
+
+      <div class="absolute top-4 right-4 bg-purple-100 text-purple-600 text-sm px-3 py-1 rounded">
+        50
+      </div>
+
+      <div class="text-purple-600 text-5xl mb-6">🗂️</div>
+
+      <h3 class="font-semibold text-gray-800">
+        Administrative
+      </h3>
+    </a>  
+
+    
+  </div>
+
     </section>
   </div>
 </template>
