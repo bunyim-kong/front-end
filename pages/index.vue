0b6e78291f6092bc3f04a59be4765cc52e8f826c
yim <bunyimkong@gmail.com>
2026-02-19 09:06:00 +0700

    feat: home page 90% by bunyim

--- pages/index.vue
+++ pages/index.vue
@@ -101,6 +101,149 @@
       
     </section>
 
+    <section>
+      <h1 class="text-4xl font-extrabold text-center text-slate-900 mt-12 mb-12">Featured Job Categories</h1>
+      
+      <div class="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6 mt-10">
+          <NuxtLink to="/sales" 
+            class="relative bg-white border border-gray-200 rounded-lg p-6 hover:shadow-md transition">
+            <div class="absolute top-4 right-4 bg-purple-100 text-purple-600 text-sm px-3 py-1 rounded">
+              583
+            </div>
+            <div class="text-purple-600 text-5xl mb-6">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-10">
+                  <path fill-rule="evenodd" d="M7.5 5.25a3 3 0 0 1 3-3h3a3 3 0 0 1 3 3v.205c.933.085 1.857.197 2.774.334 1.454.218 2.476 1.483 2.476 2.917v3.033c0 1.211-.734 2.352-1.936 2.752A24.726 24.726 0 0 1 12 15.75c-2.73 0-5.357-.442-7.814-1.259-1.202-.4-1.936-1.541-1.936-2.752V8.706c0-1.434 1.022-2.7 2.476-2.917A48.814 48.814 0 0 1 7.5 5.455V5.25Zm7.5 0v.09a49.488 49.488 0 0 0-6 0v-.09a1.5 1.5 0 0 1 1.5-1.5h3a1.5 1.5 0 0 1 1.5 1.5Zm-3 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clip-rule="evenodd" />
+                  <path d="M3 18.4v-2.796a4.3 4.3 0 0 0 .713.31A26.226 26.226 0 0 0 12 17.25c2.892 0 5.68-.468 8.287-1.335.252-.084.49-.189.713-.311V18.4c0 1.452-1.047 2.728-2.523 2.923-2.12.282-4.282.427-6.477.427a49.19 49.19 0 0 1-6.477-.427C4.047 21.128 3 19.852 3 18.4Z" />
+              </svg>
+            </div>
+            <h3 class="font-semibold text-gray-800">Sales, Business Development</h3>
+          </NuxtLink>
+
+          <NuxtLink to="/finance" 
+            class="relative bg-white border border-gray-200 rounded-lg p-6 hover:shadow-md transition">
+            <div class="absolute top-4 right-4 bg-purple-100 text-purple-600 text-sm px-3 py-1 rounded">
+              234
+            </div>
+            <div class="text-purple-600 text-5xl mb-6">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-10">
+                <path d="M10.464 8.746c.227-.18.497-.311.786-.394v2.795a2.252 2.252 0 0 1-.786-.393c-.394-.313-.546-.681-.546-1.004 0-.323.152-.691.546-1.004ZM12.75 15.662v-2.824c.347.085.664.228.921.421.427.32.579.686.579.991 0 .305-.152.671-.579.991a2.534 2.534 0 0 1-.921.42Z" />
+                <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v.816a3.836 3.836 0 0 0-1.72.756c-.712.566-1.112 1.35-1.112 2.178 0 .829.4 1.612 1.113 2.178.502.4 1.102.647 1.719.756v2.978a2.536 2.536 0 0 1-.921-.421l-.879-.66a.75.75 0 0 0-.9 1.2l.879.66c.533.4 1.169.645 1.821.75V18a.75.75 0 0 0 1.5 0v-.81a4.124 4.124 0 0 0 1.821-.749c.745-.559 1.179-1.344 1.179-2.191 0-.847-.434-1.632-1.179-2.191a4.122 4.122 0 0 0-1.821-.75V8.354c.29.082.559.213.786.393l.415.33a.75.75 0 0 0 .933-1.175l-.415-.33a3.836 3.836 0 0 0-1.719-.755V6Z" clip-rule="evenodd" />
+              </svg>
+
+            </div>
+            <h3 class="font-semibold text-gray-800">Finance, Accounting, Audit</h3>
+          </NuxtLink>
+
+          <NuxtLink to="/it" 
+            class="relative bg-white border border-gray-200 rounded-lg p-6 hover:shadow-md transition">
+            <div class="absolute top-4 right-4 bg-purple-100 text-purple-600 text-sm px-3 py-1 rounded">
+              227
+            </div>
+            <div class="text-purple-600 text-5xl mb-6">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-10">
+                <path fill-rule="evenodd" d="M2.25 6a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V6Zm3.97.97a.75.75 0 0 1 1.06 0l2.25 2.25a.75.75 0 0 1 0 1.06l-2.25 2.25a.75.75 0 0 1-1.06-1.06l1.72-1.72-1.72-1.72a.75.75 0 0 1 0-1.06Zm4.28 4.28a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z" clip-rule="evenodd" />
+              </svg>
+            </div>
+            <h3 class="font-semibold text-gray-800">IT Hardware, Software</h3>
+          </NuxtLink>
+
+          <NuxtLink to="/customer" 
+            class="relative bg-white border border-gray-200 rounded-lg p-6 hover:shadow-md transition">
+            <div class="absolute top-4 right-4 bg-purple-100 text-purple-600 text-sm px-3 py-1 rounded">
+              217
+            </div>
+            <div class="text-purple-600 text-5xl mb-6">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-10">
+                <path fill-rule="evenodd" d="M4.848 2.771A49.144 49.144 0 0 1 12 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 0 1-3.476.383.39.39 0 0 0-.297.17l-2.755 4.133a.75.75 0 0 1-1.248 0l-2.755-4.133a.39.39 0 0 0-.297-.17 48.9 48.9 0 0 1-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97ZM6.75 8.25a.75.75 0 0 1 .75-.75h9a.75.75 0 0 1 0 1.5h-9a.75.75 0 0 1-.75-.75Zm.75 2.25a.75.75 0 0 0 0 1.5H12a.75.75 0 0 0 0-1.5H7.5Z" clip-rule="evenodd" />
+              </svg>
+
+            </div>
+            <h3 class="font-semibold text-gray-800">Customer Service, Support</h3>
+          </NuxtLink>
+
+          <NuxtLink to="/marketing" 
+            class="relative bg-white border border-gray-200 rounded-lg p-6 hover:shadow-md transition">
+            <div class="absolute top-4 right-4 bg-purple-100 text-purple-600 text-sm px-3 py-1 rounded">
+              168
+            </div>
+            <div class="text-purple-600 text-5xl mb-6">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-10">
+                <path fill-rule="evenodd" d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z" clip-rule="evenodd" />
+              </svg>
+            </div>
+            <h3 class="font-semibold text-gray-800">Marketing, Media, Creative</h3>
+          </NuxtLink>
+
+          <NuxtLink to="/logistics" 
+            class="relative bg-white border border-gray-200 rounded-lg p-6 hover:shadow-md transition">
+            <div class="absolute top-4 right-4 bg-purple-100 text-purple-600 text-sm px-3 py-1 rounded">
+              70
+            </div>
+            <div class="text-purple-600 text-5xl mb-6">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-10">
+                <path d="M3.375 4.5C2.339 4.5 1.5 5.34 1.5 6.375V13.5h12V6.375c0-1.036-.84-1.875-1.875-1.875h-8.25ZM13.5 15h-12v2.625c0 1.035.84 1.875 1.875 1.875h.375a3 3 0 1 1 6 0h3a.75.75 0 0 0 .75-.75V15Z" />
+              </svg>
+            </div>
+            <h3 class="font-semibold text-gray-800">Logistics, Warehousing, Port</h3>
+          </NuxtLink>
+
+          <NuxtLink to="/hr" 
+            class="relative bg-white border border-gray-200 rounded-lg p-6 hover:shadow-md transition">
+            <div class="absolute top-4 right-4 bg-purple-100 text-purple-600 text-sm px-3 py-1 rounded">
+              69
+            </div>
+            <div class="text-purple-600 text-5xl mb-6">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-10">
+                <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
+              </svg>
+            </div>
+            <h3 class="font-semibold text-gray-800">HR, Training and Recruitment</h3>
+          </NuxtLink>
+
+          <NuxtLink to="/engineering" 
+            class="relative bg-white border border-gray-200 rounded-lg p-6 hover:shadow-md transition">
+            <div class="absolute top-4 right-4 bg-purple-100 text-purple-600 text-sm px-3 py-1 rounded">
+              63
+            </div>
+            <div class="text-purple-600 text-5xl mb-6">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-10">
+                <path fill-rule="evenodd" d="M12 6.75a5.25 5.25 0 0 1 6.775-5.025.75.75 0 0 1 .313 1.248l-3.32 3.319c.063.475.276.934.641 1.299.365.365.824.578 1.3.64l3.318-3.319a.75.75 0 0 1 1.248.313 5.25 5.25 0 0 1-5.472 6.756c-1.018-.086-1.87.1-2.309.634L7.344 21.3A3.298 3.298 0 1 1 2.7 16.657l8.684-7.151c.533-.44.72-1.291.634-2.309A5.342 5.342 0 0 1 12 6.75ZM4.117 19.125a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75h-.008a.75.75 0 0 1-.75-.75v-.008Z" clip-rule="evenodd" />
+                <path d="m10.076 8.64-2.201-2.2V4.874a.75.75 0 0 0-.364-.643l-3.75-2.25a.75.75 0 0 0-.916.113l-.75.75a.75.75 0 0 0-.113.916l2.25 3.75a.75.75 0 0 0 .643.364h1.564l2.062 2.062 1.575-1.297Z" />
+                <path fill-rule="evenodd" d="m12.556 17.329 4.183 4.182a3.375 3.375 0 0 0 4.773-4.773l-3.306-3.305a6.803 6.803 0 0 1-1.53.043c-.394-.034-.682-.006-.867.042a.589.589 0 0 0-.167.063l-3.086 3.748Zm3.414-1.36a.75.75 0 0 1 1.06 0l1.875 1.876a.75.75 0 1 1-1.06 1.06L15.97 17.03a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+              </svg>
+            </div>
+            <h3 class="font-semibold text-gray-800">Engineering, Technical, HSE</h3>
+          </NuxtLink>
+
+          <NuxtLink to="/driver" 
+            class="relative bg-white border border-gray-200 rounded-lg p-6 hover:shadow-md transition">
+            <div class="absolute top-4 right-4 bg-purple-100 text-purple-600 text-sm px-3 py-1 rounded">
+              50
+            </div>
+            <div class="text-purple-600 text-5xl mb-6">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-10">
+                <path d="M10.5 18.75a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z" />
+                <path fill-rule="evenodd" d="M8.625.75A3.375 3.375 0 0 0 5.25 4.125v15.75a3.375 3.375 0 0 0 3.375 3.375h6.75a3.375 3.375 0 0 0 3.375-3.375V4.125A3.375 3.375 0 0 0 15.375.75h-6.75ZM7.5 4.125C7.5 3.504 8.004 3 8.625 3H9.75v.375c0 .621.504 1.125 1.125 1.125h2.25c.621 0 1.125-.504 1.125-1.125V3h1.125c.621 0 1.125.504 1.125 1.125v15.75c0 .621-.504 1.125-1.125 1.125h-6.75A1.125 1.125 0 0 1 7.5 19.875V4.125Z" clip-rule="evenodd" />
+              </svg>
+            </div>
+            <h3 class="font-semibold text-gray-800">Driver, Delivery, Motorbike</h3>
+          </NuxtLink>
+
+          <NuxtLink to="/admin" 
+            class="relative bg-white border border-gray-200 rounded-lg p-6 hover:shadow-md transition">
+            <div class="absolute top-4 right-4 bg-purple-100 text-purple-600 text-sm px-3 py-1 rounded">
+              50
+            </div>
+            <div class="text-purple-600 text-5xl mb-6">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-10">
+                <path d="M19.5 21a3 3 0 0 0 3-3v-4.5a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3V18a3 3 0 0 0 3 3h15ZM1.5 10.146V6a3 3 0 0 1 3-3h5.379a2.25 2.25 0 0 1 1.59.659l2.122 2.121c.14.141.331.22.53.22H19.5a3 3 0 0 1 3 3v1.146A4.483 4.483 0 0 0 19.5 9h-15a4.483 4.483 0 0 0-3 1.146Z" />
+              </svg>
+            </div>
+            <h3 class="font-semibold text-gray-800">Administrative</h3>
+          </NuxtLink>  
+        </div>
+    </section>
+
      <!-- company section -->
     <section class="py-16 bg-white">
       <div class="container mx-auto px-4">
